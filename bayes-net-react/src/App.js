4d39cd6a9749a05bb272d72b528c3fcee239da68
Yosi Hammer <yosi.hammer@skf.com>
2021-10-19 22:58:50 +0300

    experimenting with random var definition

--- bayes-net-react/src/App.js
+++ bayes-net-react/src/App.js
@@ -1,29 +1,17 @@
 import './App.css';
 import Button from '@mui/material/Button';
 import ReactFlow from 'react-flow-renderer';
+import {CategoricalVariable} from './RandVar';
+
+
+var v1 = new CategoricalVariable('Rain', 
+  'will it rain tomorrow', 
+  ['yes', 'no'],
+  [0.5, 0.5]
+);
 
 const elements = [
-  {
-    id: '1',
-    type: 'input', // input node
-    data: { label: 'Input Node' },
-    position: { x: 250, y: 25 },
-  },
-  // default node
-  {
-    id: '2',
-    // you can also pass a React component as a label
-    data: { label: <div>Default Node</div> },
-    position: { x: 100, y: 125 },
-  },
-  {
-    id: '3',
-    type: 'output', // output node
-    data: { label: 'Output Node' },
-    position: { x: 250, y: 250 },
-  },
-  // animated edge
-  { id: 'e2-3', source: '2', target: '3' },
+ v1.element,
 ];
 
 export default () => (
--- bayes-net-react/src/RandVar.js
+++ bayes-net-react/src/RandVar.js
@@ -1,13 +1,39 @@
-class CategoricalVariable {
-    constructor(values, probs) {
+import { positions } from "@mui/system";
+
+class RandVar {
+    constructor(name, description) {
+        this.description = description;
+        this.name = name;
+    }
+
+    get element() {
+        return {
+            id: this.name,
+            type: 'default',
+            data: {
+                label: this.name
+            },
+            position: {x : 0, y: 0} 
+        }
+    }
+}
+
+
+export class CategoricalVariable extends RandVar {
+    constructor(name, description, values, probs) {
+        super(name, description);
         if (!(values instanceof Array)) {
-            throw TypeError('values should be an array of possible values')
+            throw TypeError('for a categorival variable ' +
+                'values should be an array of possible values')
         }
         if (!(probs instanceof Array)) {
-            throw TypeError('probs should be an array of possible values')
+            throw TypeError('for a categorical vairable probs ' +
+                'should be an array of possible values')
         }
         if (!(probs.length == values.length)) {
-            throw Error('probs and values should be of the same length')
+            throw Error('probs and values of a categorical variable ' +
+            'should be of the same length, probs = ' + probs + 
+            ', values = ' + values)
         }
         this.values = values;
         var probSum = probs.reduce((a,b) => a + b, 0);
